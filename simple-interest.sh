fa6dcfe082af699e4be70fe9dcca0bf370d224f7
Kaung Htet <kaunghtet061097@gmail.com>
2026-07-29 23:01:41 +0630

    Update author information in simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,9 +4,9 @@
 
    # Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
+   # Author: Kaung Htet
    # Additional Authors:
-   # <your GitHub username>
+   # <KaungHtet6107>
 
    # Input:
    # p, principal amount
